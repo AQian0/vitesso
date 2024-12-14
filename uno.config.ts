0c0a0e8220a5d23978d12948f7186b2edbbda3df
AQian <970506352@qq.com>
2024-12-15 00:51:30 +0800

    init

---
+++ uno.config.ts
@@ -0,0 +1,21 @@
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetUno,
+  presetWebFonts,
+} from 'unocss'
+
+export default defineConfig({
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons({
+      scale: 1.2,
+      warn: true,
+    }),
+    presetWebFonts({
+      provider: 'none',
+    }),
+  ],
+})
